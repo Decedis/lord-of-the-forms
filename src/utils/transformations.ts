f249b75ed061b7e69208c8544e7c88647f5d8233
Steven Brcak <sbrcak@live.com>
2023-10-11 00:16:10 -0700

    Transformation for name and phone created. Working as intended.

--- src/utils/transformations.ts
+++ src/utils/transformations.ts
@@ -1,9 +1,26 @@
-export const capitalize = () => {
-    // todo: build this function
-    // `capitalize("jOn")` should output `"Jon"`
-}
+export const capitalize = (input: string): string => {
+  // todo: build this function
+  // `capitalize("jOn")` should output `"Jon"`
+  let result = "";
+  for (let i = 0; i < input.length; i++) {
+    if (i === 0) {
+      result += input[i].toLocaleUpperCase();
+    } else {
+      result += input[i].toLocaleLowerCase();
+    }
+  }
+  return result;
+};
 
-export const formatPhoneNumber = () => {
-    // todo: build this function
-    // `formatPhoneNumber("1234567")` should be `"12-34-56-7"`
-}
+export const formatPhoneNumber = (input: string): string => {
+  // todo: build this function
+  // `formatPhoneNumber("1234567")` should be `"12-34-56-7"`
+  let result = "";
+  for (let i = 0; i < input.length; i++) {
+    result += input[i];
+    if (i === 1 || i === 3 || i === 5) {
+      result += "-";
+    }
+  }
+  return result;
+};
