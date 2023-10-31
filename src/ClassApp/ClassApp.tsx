e58f020f8dd73d0d0abe1d9ac6bd8a8747d1416a
Steven Brcak <sbrcak@live.com>
2023-10-30 18:10:01 -0700

    Remove unused defaultUser in ClassApp

--- src/ClassApp/ClassApp.tsx
+++ src/ClassApp/ClassApp.tsx
@@ -4,14 +4,6 @@ import { UserInformation } from "../types";
 import { ProfileInformation } from "../ProfileInformation";
 type State = { userInformation: UserInformation | null };
 
-const defaultUser: UserInformation = {
-  email: "default@default.com",
-  firstName: "Default",
-  lastName: "Default",
-  phone: "1234567",
-  city: "Hobbiton",
-};
-
 export class ClassApp extends Component<Record<string, never>, State> {
   state = { userInformation: null };
 
